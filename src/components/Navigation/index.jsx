2153a1463c157daa618e3fc71a742ef2016e54ff
duong bao tran <bao.tran@m1studio.co>
2019-09-17 16:06:58 +0700

    session handling

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
+import {AuthUserContext} from '../Session';
+
 
 const Navigation = () => (
   <div>
+    <AuthUserContext.Consumer>
+     {authUser => 
+       authUser ? <NavigationAuth/> : <NavigationNonAuth/>
+     }
+    </AuthUserContext.Consumer>
+  </div>
+)
+
+const NavigationAuth = () => (
+  <div>
     <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
       <li>
         <Link to={ROUTES.LANDING}>Landing</Link>
       </li>
@@ -19,7 +29,20 @@ const Navigation = () => (
         <Link to={ROUTES.ACCOUNT}>Account</Link>
       </li>
       <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
+        <SignOutButton/>
+      </li>
+    </ul>
+  </div>
+);
+
+const NavigationNonAuth = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
       </li>
     </ul>
   </div>

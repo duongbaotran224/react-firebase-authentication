8cb77e882fffd25c861379c05e690aff2c4b35c7
duong bao tran <bao.tran@m1studio.co>
2019-09-29 13:05:28 +0700

    up

--- src/components/Session/withAuthentication.jsx
+++ src/components/Session/withAuthentication.jsx
@@ -15,7 +15,7 @@ const withAuthentication = Component => {
     }
     
     componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChanged(
+      this.listener = this.props.firebase.onAuthUserListener(
         authUser => this.setState({ authUser }),
         () => this.setState({ authUser: null }),
       );

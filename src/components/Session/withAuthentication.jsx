b74158c1d392f741231ab5b1453099dcab07ac44
duong bao tran <bao.tran@m1studio.co>
2019-10-03 19:45:55 +0700

    session persistence (localstorage)

--- src/components/Session/withAuthentication.jsx
+++ src/components/Session/withAuthentication.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import {withFirebase} from '../Firebase';
 import {AuthUserContext} from '../Session';
+import { stringify } from 'querystring';
 
 const withAuthentication = Component => {
   class WithAuthentication extends React.Component {
@@ -9,14 +10,20 @@ const withAuthentication = Component => {
       super(props)
     
       this.state = {
-         authUser: null
+         authUser: JSON.parse(localStorage.getItem('authUser'))
       }
     }
     
     componentDidMount() {
       this.listener = this.props.firebase.onAuthUserListener(
-        authUser => this.setState({ authUser }),
-        () => this.setState({ authUser: null }),
+        authUser => {
+          localStorage.setItem('authUser', JSON.stringify(authUser))
+          this.setState({ authUser })
+        },
+        () => {
+          localStorage.removeItem('authUser')
+          this.setState({ authUser: null })
+        }
       );
     }
 

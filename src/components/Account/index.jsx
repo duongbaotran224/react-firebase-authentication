52cc3e7300d6d26bdc4e13b2814ee5b359dfbac1
duong bao tran <bao.tran@m1studio.co>
2019-10-16 09:58:37 +0700

    link social logins to one account

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import {PasswordChangeForm} from '../PasswordChange'
 import {PasswordForgetForm} from '../PasswordForget'
 import {withAuthorization, AuthUserContext} from '../Session';
+import {withFirebase} from '../Firebase';
+
+const SIGN_IN_METHODS = [
+  {
+    id: 'password',
+    provider: null,
+  },
+  {
+    id: 'google.com',
+    provider: 'googleProvider',
+  },
+  {
+    id: 'facebook.com',
+    provider: 'facebookProvider',
+  },
+  {
+    id: 'twitter.com',
+    provider: 'twitterProvider',
+  },
+];
 
 const AccountPage = () => (
   <AuthUserContext.Consumer>
@@ -11,10 +31,205 @@ const AccountPage = () => (
         <h1>Account: {authUser.email}</h1>
         <PasswordChangeForm/>
         <PasswordForgetForm/>
+        <LoginManagement authUser={authUser} />
       </div>
     )}
   </AuthUserContext.Consumer>
 );
+
+
+class LoginManagementBase extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      activeSignInMethods: [],
+      error: null,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchSignInMethods()
+  }
+  
+
+  fetchSignInMethods = () => {
+    this.props.firebase.auth
+      .fetchSignInMethodsForEmail(this.props.authUser.email)
+      .then(activeSignInMethods => {
+        this.setState({
+          activeSignInMethods,
+          error: null
+        })
+      })
+      .catch(error => {
+        this.setState({error})
+      })
+  };
+
+  onSocialLoginLink = provider => {
+    // console.log(this.props.firebase)
+    this.props.firebase.auth.currentUser
+      .linkWithPopup(this.props.firebase[provider])
+      .then(this.fetchSignInMethods)
+      .catch(error => {
+        this.setState({error})
+      })
+
+  };
+
+  onUnLink = providerId => {
+    this.props.firebase.auth.currentUser
+      .unlink(providerId)
+      .then(this.fetchSignInMethods)
+      .catch(error => {
+        this.setState({error})
+      })
+
+  }
+
+  onDefaultLoginLink = (password) => {
+    const credential = this.props.firebase.emailAuthProvider.credential(
+      this.props.authUser.email,
+      password
+    )
+    
+    this.props.firebase.auth.currentUser
+      .linkAndRetrieveDataWithCredential(credential)
+      .then(this.fetchSignInMethods)
+      .catch(error => {
+        this.setState({error})
+      })
+  };
+  
+  render() {
+    const { activeSignInMethods, error } = this.state;
+
+    return (
+      <div>
+        Sign In Methods: 
+        <ul>
+          {SIGN_IN_METHODS.map(signInMethod => {
+
+            const onlyOneLeft = activeSignInMethods.length === 1;            
+            const isEnabled = activeSignInMethods.includes(
+              signInMethod.id
+            );
+
+            return (
+              <li key={signInMethod.id}>
+
+                {
+                  signInMethod.id === 'password' ? (
+                    <DefaultLoginToggle
+                      onlyOneLeft={onlyOneLeft}
+                      isEnabled={isEnabled}
+                      signInMethod={signInMethod}
+                      onLink={this.onDefaultLoginLink}
+                    />
+                  ) : (
+                    <SocialLoginToggle
+                      onlyOneLeft={onlyOneLeft}
+                      isEnabled={isEnabled}
+                      signInMethod={signInMethod}
+                      onLink={this.onSocialLoginLink}
+                      onUnLink={this.onUnLink}
+                    />
+                  )
+                }
+                
+              </li>
+            ) 
+          })}
+        </ul>
+        {error && error.message}
+      </div>
+    )
+  }
+}
+
+
+class DefaultLoginToggle extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      passwordOne: "",
+      passwordTwo: "",
+    }
+  }
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmit = event => {
+    this.setState({
+      passwordOne: "" ,
+      passwordTwo: "" ,
+    })
+    this.props.onLink(this.state.passwordOne)
+    event.preventDefault()
+  }
+
+  render() {
+    const {isEnabled, signInMethod, onUnLink, onlyOneLeft} = this.props
+    const {passwordOne, passwordTwo} = this.state
+    const isInvalid = passwordOne !== passwordTwo || passwordOne === "" ;
+
+    return isEnabled ? (
+      <button
+        type="button"
+        onClick={() => onUnLink(signInMethod.id)}
+        disabled={onlyOneLeft}
+      >
+        Deactivate {signInMethod.id}
+      </button>
+    ) : (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="New Password"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm New Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Link {signInMethod.id}
+        </button>
+      </form>
+    )
+  }
+};
+
+
+const SocialLoginToggle = ({isEnabled, signInMethod,onLink, onUnLink, onlyOneLeft}) => (
+  isEnabled ? ( 
+    <button type="button" 
+            onClick={() => onUnLink(signInMethod.id)}
+            disabled={onlyOneLeft}
+    >
+      Deactive {signInMethod.id}
+    </button>
+  ) : (
+    <button type="button" 
+            onClick={() => onLink(signInMethod.provider)}
+    >
+      Link {signInMethod.id}
+    </button>
+  )
+);
+
+const LoginManagement = withFirebase(LoginManagementBase);
 
 const condition = authUser => !!authUser;
 
--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -25,6 +25,16 @@ const INITIAL_STATE = {
   error: null,
 };
 
+const ERROR_CODE_ACCOUNT_EXISTS =
+'auth/account-exists-with-different-credential';
+
+const ERROR_MSG_ACCOUNT_EXISTS = `
+An account with an E-Mail address to
+this social account already exists. Try to login from
+this account instead and associate your social accounts on
+your personal account page.
+`;
+
 class SignInFormBase extends Component {
   constructor(props) {
     super(props)
@@ -114,6 +124,9 @@ class SignInGoogleBase extends Component {
         this.props.history.push(ROUTES.HOME)
       })
       .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
         this.setState({error})
       });
 
@@ -148,8 +161,8 @@ class SignInFacebookBase extends Component {
       this.props.firebase
         .user(socialAuthUser.user.uid)
         .set({
-          username: socialAuthUser.user.displayName,
-          email: socialAuthUser.user.email,
+          username: socialAuthUser.additionalUserInfo.profile.name,
+          email: socialAuthUser.additionalUserInfo.profile.email,
           roles: {}
         })
     })
@@ -158,6 +171,9 @@ class SignInFacebookBase extends Component {
       this.props.history.push(ROUTES.HOME)
     })
     .catch(error => {
+      if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+      }
       this.setState({error})
     })
 
@@ -190,8 +206,8 @@ class SignInTwitterBase extends Component {
         return this.props.firebase
           .user(socialAuthUser.user.uid)
           .set({
-            username: socialAuthUser.user.displayName,
-            email: socialAuthUser.user.email,
+            username: socialAuthUser.additionalUserInfo.profile.name,
+            email: socialAuthUser.additionalUserInfo.profile.email,
             roles: {}
           })
       })
@@ -199,6 +215,9 @@ class SignInTwitterBase extends Component {
         this.setState({error: null})
       })
       .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
         this.setState({error})
       })
 
